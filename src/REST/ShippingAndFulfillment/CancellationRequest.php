a791b74dafc065bbe5191e3e11b218fb9fb1b576
朱法贵 <verdient@yicencorp.com>
2022-12-29 11:44:14 +0800

    新增取消请求

---
+++ src/REST/ShippingAndFulfillment/CancellationRequest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Verdient\Shopify\REST\ShippingAndFulfillment;
+
+use Verdient\Shopify\REST\AbstractComponent;
+use Verdient\Shopify\REST\Response;
+use Verdient\Shopify\REST\Traits\HasCreate;
+
+/**
+ * 取消请求
+ * @author Verdient。
+ */
+class CancellationRequest extends AbstractComponent
+{
+    use HasCreate;
+
+    /**
+     * @var int 履约单编号
+     * @author Verdient。
+     */
+    protected $fulfillmentOrderId = null;
+
+    /**
+     * @inheritdoc
+     * @param int $fulfillmentOrderId 履约单编号
+     * @author Verdient。
+     */
+    public function __construct($fulfillmentOrderId, $host, $accessToken)
+    {
+        $this->fulfillmentOrderId = $fulfillmentOrderId;
+        parent::__construct($host, $accessToken);
+    }
+
+    /**
+     * @inheritdoc
+     * @author Verdient。
+     */
+    protected function resource(): string
+    {
+        return 'fulfillment_orders/' . $this->fulfillmentOrderId . '/cancellation_request';
+    }
+
+    /**
+     * 接受
+     * @return Response
+     * @author Verdient。
+     */
+    public function accept(): Response
+    {
+        return $this
+            ->request($this->resource() . '/accept')
+            ->setMethod('POST')
+            ->send();
+    }
+
+    /**
+     * 拒绝
+     * @return Response
+     * @author Verdient。
+     */
+    public function reject(): Response
+    {
+        return $this
+            ->request($this->resource() . '/reject')
+            ->setMethod('POST')
+            ->send();
+    }
+}
